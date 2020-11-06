4b93f9b6509a2d81fa426624313c39f01f5d7900
Charles-DJM <charlesdjfc@gmail.com>
2020-11-06 11:49:40 +0100

    PacManEvents

--- src/Gameplay/PacManEvents.java
+++ src/Gameplay/PacManEvents.java
@@ -1,4 +1,10 @@
 package Gameplay;
 
-public interface PacManEvents {
+import Engine.Events;
+import javafx.event.EventHandler;
+
+import java.awt.event.KeyEvent;
+
+public class PacManEvents implements Events {
+    EventHandler<KeyEvent> eventHandler;
 }
