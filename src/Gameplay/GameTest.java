bc2038a97f2813de473942f51ae5cbee2e796c2c
Auhalf <leah.tapin@orange.fr>
2020-11-15 15:16:38 +0100

    Ajout de la classe GameTest.java

---
+++ src/Gameplay/GameTest.java
@@ -0,0 +1,33 @@
+package Gameplay;
+
+import Engine.GraphicsEngine;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void init() {
+        GraphicsEngine graphicsEngine = new GraphicsEngine();
+        Game game = new Game(graphicsEngine);
+        game.init();
+        assertEquals(game.getEntities().get(0), game.getCharacter());
+        double[] expectedPosition = new double[]{600.0, 400.0};
+        double[] expectedSpeed = new double[]{0.0, 0.0};
+        double[] expectedDimensions = new double[]{50.0, 50.0};
+        String expectedFileName = "pacman.png";
+        double[] actualPosition = game.getCharacter().getPosition().stream().mapToDouble(Double::doubleValue).toArray();
+        double[] actualSpeed = game.getCharacter().getSpeed().stream().mapToDouble(Double::doubleValue).toArray();
+        double[] actualDimensions = game.getCharacter().getDimensions().stream().mapToDouble(Double::doubleValue).toArray();
+        String actualFileName = game.getCharacter().getImage();
+        assertEquals(expectedFileName, actualFileName);
+        assertArrayEquals(expectedPosition, actualPosition);
+        assertArrayEquals(expectedDimensions, actualDimensions);
+        assertArrayEquals(expectedSpeed, actualSpeed);
+    }
+}
